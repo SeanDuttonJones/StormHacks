22e631f140b2bae9a353e6f29b54ff67ca67c5db
nsimms <nsimms@sfu.ca>
2021-02-20 15:14:23 -0800

    add blog app

--- VVS/Blog/urls.py
+++ VVS/Blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views as home_views
+from . import views as blog_views
 
 urlpatterns = [
     ]
--- VVS/Website_Settings/urls.py
+++ VVS/Website_Settings/urls.py
@@ -20,4 +20,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
+    path('blog/', include("Blog.urls")),
 ]
