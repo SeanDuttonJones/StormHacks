dc143a324b49ded6f2bf99d428e6b7f4f09139c2
P-Sood <55671093+P-Sood@users.noreply.github.com>
2021-02-20 14:14:35 -0800

    Changed Settings folder name

--- VVS/Website_Settings/settings.py
+++ VVS/Website_Settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'VVS.urls'
+ROOT_URLCONF = 'Website_Settings.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'VVS.wsgi.application'
+WSGI_APPLICATION = 'Website_Settings.wsgi.application'
 
 
 # Database
