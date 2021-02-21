db38785a808f8b3df55b858c05ff84b249bf341f
Jaskaran Dhanoa <jaskaran@Jaskarans-MacBook-Pro.local>
2021-02-20 17:06:34 -0800

    some sending link

--- VVS/Home/urls.py
+++ VVS/Home/urls.py
@@ -3,4 +3,5 @@ from . import views as home_views
 
 urlpatterns = [
     path('', home_views.home, name='homepage'),
+    path('about/', home_views.about, name='aboutpage'),
 ]
--- VVS/Home/views.py
+++ VVS/Home/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def home(request):
     
     return render(request,'Home/home.html')
+def about(request):
+    
+    return render(request,'Home/about.html')
