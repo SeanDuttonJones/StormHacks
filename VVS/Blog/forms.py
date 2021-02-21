e09550efb29b703d942f28a1018cd9a0a238c253
Jaskaran Dhanoa <jaskaran@Jaskarans-MacBook-Pro.local>
2021-02-20 21:22:11 -0800

    latest

--- VVS/Blog/forms.py
+++ VVS/Blog/forms.py
@@ -1,9 +1,41 @@
 # forms.py 
-from django import forms 
-from .models import *
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
   
+
+
 class PostForm(forms.ModelForm): 
-  
-    class Meta: 
-        model = Post 
-        fields = ['title','content','date_posted','img'] 
+    """
+    Logging into Website Form
+    """
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(max_length=100)
+    
+    date_posted = forms.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    img = forms.ImageField(upload_to='images/')
+
+    def __init__(self,args,**kwargs):
+        super().__init__(args,**kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout (
+            Fieldset(
+                " ",
+                "title",
+                "content",
+                "date_posted",
+                "author",
+                "img"
+            ),
+            ButtonHolder(
+                Submit('submit','Login Data',css_class='button white'),
+
+            ),
+
+        )
--- VVS/Blog/models.py
+++ VVS/Blog/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Post(models.Model):
-    title = models.TextField(max_length=100)
-    content = models.TextField()
-    # lets us update date, and take timezone into consideration
-    date_posted = models.DateField(default=timezone.now)
-    # get from db and if user deleted delete their posts
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='images/')
+# class Post(models.Model):
+#     title = models.TextField(max_length=100)
+#     content = models.TextField()
+#     # lets us update date, and take timezone into consideration
+#     date_posted = models.DateField(default=timezone.now)
+#     # get from db and if user deleted delete their posts
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     img = models.ImageField(upload_to='images/')
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
