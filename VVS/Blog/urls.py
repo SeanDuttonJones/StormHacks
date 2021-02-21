2c9dfb5b87ebf9691e4e477f642ef996bbc9aa60
nsimms <nsimms@sfu.ca>
2021-02-20 16:40:41 -0800

    add base css style for blog posts

--- VVS/Blog/urls.py
+++ VVS/Blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views as blog_views
+# followed tutorial: https://www.youtube.com/watch?v=qDwdMDQ8oX4&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=3
+# for blog creation html, views and other set up for the blog
+# used to learn jango and how to use html with it
 
 urlpatterns = [
     path('', blog_views.home, name='blog-home'),
