9d9f347aea44942926a4cdee448d4d272fd35394
nsimms <nsimms@sfu.ca>
2021-02-20 19:14:41 -0800

    add side bar from main

--- VVS/Blog/models.py
+++ VVS/Blog/models.py
@@ -10,5 +10,9 @@ class Post(models.Model):
     # lets us update date, and take timezone into consideration
     date_posted = models.DateField(default=timezone.now)
     # get from db and if user deleted delete their posts
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = 'Nate the Great'
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
--- VVS/Home/models.py
+++ VVS/Home/models.py
@@ -5,13 +5,4 @@ from django.contrib.auth.models import User
 
 
 
-class Post(models.Model):
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    date_posted=models.DateTimeField(default=timezone.now)
-    author= models.ForeignKey(User,on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='images/')
-     
-    def __str__(self):
-        return self.title
 
