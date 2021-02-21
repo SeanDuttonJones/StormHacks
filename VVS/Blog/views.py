aaaa1a752fd4d1d92154b9d7540283f690619e5a
nsimms <nsimms@sfu.ca>
2021-02-20 16:06:22 -0800

    Add html to loop through posts

--- VVS/Blog/views.py
+++ VVS/Blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 
+# test data
+posts = [
+    {
+        'author': 'Nate the Great',
+        'title': 'Why we need Fair trade',
+        'content': 'First post content',
+        'date_posted': 'January 1, 2021'
+    },
+    {
+        'author': 'Jas',
+        'title': 'Why are you helping the farmers?',
+        'content': 'Second post content',
+        'date_posted': 'January 20, 2021'
+    }
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'title': 'About'})
